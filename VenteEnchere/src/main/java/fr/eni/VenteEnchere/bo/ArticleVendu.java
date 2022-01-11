1f7c5788e55907a1b98b169eb9619e30813297d7
xMarc007x <marc.mainguy@laposte.net>
2022-01-11 14:00:04 +0100

    commit marc

---
+++ VenteEnchere/src/main/java/fr/eni/VenteEnchere/bo/ArticleVendu.java
@@ -0,0 +1,290 @@
+/**
+ * 
+ */
+package fr.eni.VenteEnchere.bo;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author mmainguy2021
+ *
+ */
+public class ArticleVendu {
+	
+	private Integer noArticle;
+	private String nonArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private String dateFinEncheres;
+	private String miseAPrix;
+	private String prixVente;
+	private String etatVente;
+	private Utilisateur user;
+	private List<Enchere> lstEncheres;
+	private Categorie categorie;
+	private Retrait retrait;
+	
+	/**
+	 * 
+	 */
+	public ArticleVendu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param noArticle
+	 * @param nonArticle
+	 * @param description
+	 * @param dateDebutEncheres
+	 * @param dateFinEncheres
+	 * @param miseAPrix
+	 * @param prixVente
+	 * @param etatVente
+	 */
+	public ArticleVendu(Integer noArticle, String nonArticle, String description, Date dateDebutEncheres,
+			String dateFinEncheres, String miseAPrix, String prixVente, String etatVente) {
+		super();
+		this.noArticle = noArticle;
+		this.nonArticle = nonArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+	/**
+	 * @param noArticle
+	 * @param nonArticle
+	 * @param description
+	 * @param dateDebutEncheres
+	 * @param dateFinEncheres
+	 * @param miseAPrix
+	 * @param prixVente
+	 * @param etatVente
+	 * @param user
+	 * @param lstEncheres
+	 * @param categorie
+	 * @param retrait
+	 */
+	public ArticleVendu(Integer noArticle, String nonArticle, String description, Date dateDebutEncheres,
+			String dateFinEncheres, String miseAPrix, String prixVente, String etatVente, Utilisateur user,
+			List<Enchere> lstEncheres, Categorie categorie, Retrait retrait) {
+		super();
+		this.noArticle = noArticle;
+		this.nonArticle = nonArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.user = user;
+		this.lstEncheres = lstEncheres;
+		this.categorie = categorie;
+		this.retrait = retrait;
+	}
+
+	/**
+	 * @return the noArticle
+	 */
+	public Integer getNoArticle() {
+		return noArticle;
+	}
+
+	/**
+	 * @param noArticle the noArticle to set
+	 */
+	public void setNoArticle(Integer noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	/**
+	 * @return the nonArticle
+	 */
+	public String getNonArticle() {
+		return nonArticle;
+	}
+
+	/**
+	 * @param nonArticle the nonArticle to set
+	 */
+	public void setNonArticle(String nonArticle) {
+		this.nonArticle = nonArticle;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the dateDebutEncheres
+	 */
+	public Date getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	/**
+	 * @param dateDebutEncheres the dateDebutEncheres to set
+	 */
+	public void setDateDebutEncheres(Date dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	/**
+	 * @return the dateFinEncheres
+	 */
+	public String getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	/**
+	 * @param dateFinEncheres the dateFinEncheres to set
+	 */
+	public void setDateFinEncheres(String dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	/**
+	 * @return the miseAPrix
+	 */
+	public String getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	/**
+	 * @param miseAPrix the miseAPrix to set
+	 */
+	public void setMiseAPrix(String miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	/**
+	 * @return the prixVente
+	 */
+	public String getPrixVente() {
+		return prixVente;
+	}
+
+	/**
+	 * @param prixVente the prixVente to set
+	 */
+	public void setPrixVente(String prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	/**
+	 * @return the etatVente
+	 */
+	public String getEtatVente() {
+		return etatVente;
+	}
+
+	/**
+	 * @param etatVente the etatVente to set
+	 */
+	public void setEtatVente(String etatVente) {
+		this.etatVente = etatVente;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public Utilisateur getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(Utilisateur user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the lstEncheres
+	 */
+	public List<Enchere> getLstEncheres() {
+		return lstEncheres;
+	}
+
+	/**
+	 * @param lstEncheres the lstEncheres to set
+	 */
+	public void setLstEncheres(List<Enchere> lstEncheres) {
+		this.lstEncheres = lstEncheres;
+	}
+
+	/**
+	 * @return the categorie
+	 */
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	/**
+	 * @param categorie the categorie to set
+	 */
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	/**
+	 * @return the retrait
+	 */
+	public Retrait getRetrait() {
+		return retrait;
+	}
+
+	/**
+	 * @param retrait the retrait to set
+	 */
+	public void setRetrait(Retrait retrait) {
+		this.retrait = retrait;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ArticleVendu [noArticle=");
+		builder.append(noArticle);
+		builder.append(", nonArticle=");
+		builder.append(nonArticle);
+		builder.append(", description=");
+		builder.append(description);
+		builder.append(", dateDebutEncheres=");
+		builder.append(dateDebutEncheres);
+		builder.append(", dateFinEncheres=");
+		builder.append(dateFinEncheres);
+		builder.append(", miseAPrix=");
+		builder.append(miseAPrix);
+		builder.append(", prixVente=");
+		builder.append(prixVente);
+		builder.append(", etatVente=");
+		builder.append(etatVente);
+		builder.append(", user=");
+		builder.append(user);
+		builder.append(", lstEncheres=");
+		builder.append(lstEncheres);
+		builder.append(", categorie=");
+		builder.append(categorie);
+		builder.append(", retrait=");
+		builder.append(retrait);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
