2506fb076895d080053bb5a4eac25668cf6e2fb7
AntoineGiusti <giusti.antoine@gmail.com>
2022-01-11 13:33:32 +0100

    update bo

--- VenteEnchere/src/main/java/fr/eni/VenteEnchere/bo/Utilisateur.java
+++ VenteEnchere/src/main/java/fr/eni/VenteEnchere/bo/Utilisateur.java
@@ -2,6 +2,16 @@ package fr.eni.VenteEnchere.bo;
 
 public class Utilisateur {
 	
+	private String noUtilisateur ;
+	private String pseudo ;
+	private String nom ;
+	private String prenom ;
+	private String email ;
+	private String telephone ;
+	private String rue ;
+	private String codePostal ;
+	private String ville ;
+	private String nom ;
 	private String nom ;
 
 }
