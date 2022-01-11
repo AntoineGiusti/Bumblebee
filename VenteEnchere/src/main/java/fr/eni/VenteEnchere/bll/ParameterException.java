b8e7f452cf7035610b78e8f748e7b503007f5dd7
Thomas-Hamon <hamon.thomas@gmail.com>
2022-01-11 15:46:30 +0100

    bll ajouter utilisateur

---
+++ VenteEnchere/src/main/java/fr/eni/VenteEnchere/bll/ParameterException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fr.eni.VenteEnchere.bll;
+
+/**
+ * @author thamon2021
+ *
+ */
+public class ParameterException extends Exception {
+	
+	public ParameterException(String message) {
+		super(message);
+	}
+
+}
