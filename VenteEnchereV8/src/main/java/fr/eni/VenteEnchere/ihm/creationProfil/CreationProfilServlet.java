856500a9680f245765b626d0dd93229bba50c7e4
Thomas-Hamon <hamon.thomas@gmail.com>
2022-01-13 13:34:38 +0100

    commit next screen

--- VenteEnchereV8/src/main/java/fr/eni/VenteEnchere/ihm/creationProfil/CreationProfilServlet.java
+++ VenteEnchereV8/src/main/java/fr/eni/VenteEnchere/ihm/creationProfil/CreationProfilServlet.java
@@ -34,6 +34,8 @@ public class CreationProfilServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
+		String nextScreen = "WEB-INF/monProfil.jsp"; 
+		
 		CreationProfilModel model = new CreationProfilModel();
 		
 //		MonProfilModel model = (MonProfilModel)request.getSession().getAttribute("model");
@@ -65,7 +67,9 @@ public class CreationProfilServlet extends HttpServlet {
 				
 				request.setAttribute("model", model);
 				
-				request.getRequestDispatcher("MaPageUtilisateurServlet").forward(request, response);
+				nextScreen = "MaPageUtilisateurServlet";
+				
+				
 				
 			} catch (BLLException e) {
 				// TODO Auto-generated catch block
@@ -74,12 +78,9 @@ public class CreationProfilServlet extends HttpServlet {
 			}
 					
 		}
-		else {
-			request.getRequestDispatcher("WEB-INF/monProfil.jsp").forward(request, response);
-		}
-			
-	
-			
+		
+			request.getRequestDispatcher(nextScreen).forward(request, response);
+		
 		}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
